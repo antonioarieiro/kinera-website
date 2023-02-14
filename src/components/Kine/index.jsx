fca51709cc2c9545742a800e502da4cbf6daa4dd
leandro <leandro.jareno@gmail.com>
2023-02-14 15:58:26 -0300

    update files2

--- src/components/Kine/index.jsx
+++ src/components/Kine/index.jsx
@@ -10,7 +10,7 @@ const Kine = () => {
                     <img src={img08} alt="claquete" className="w-3/4 animate-bounce" />
                 </div>
                 
-                <div className="md:w-1/2 w-full" data-aos="fade-left">
+                <div className="md:w-1/2 w-full -mt-[200px] " data-aos="fade-left">
                     <p className="text-[24px] md:text-left text-center my-10 text-[#35012B]"><strong>Kine</strong> is developed using a <strong>substrate-based blockchain</strong> infrastructure that makes it interoperable with the <strong>Kusama</strong> and <strong>Polkadot</strong> networks.</p>
                     <p className="text-[24px] md:text-left text-center my-10 text-[#35012B]">Kine is a <strong>token</strong> created to <strong>rank film</strong> and <strong>video</strong>, <strong>curate Constellations’ categories</strong>, <strong>curate</strong> the <strong>See Channels</strong>, and <strong>films</strong> and <strong>videos</strong> that the user thinks to <strong>deserve</strong> to be <strong>produced</strong> in the <strong>Producers Network</strong> and to make <strong>governance decisions</strong>.</p>
                 </div>
