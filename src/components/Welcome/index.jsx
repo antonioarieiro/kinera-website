d0f457a1db63b285c941b088d4dbca9135873fb4
gitlab_kine_leandro <leandro@ipa.kine.network>
2023-09-13 17:01:22 -0300

    changes

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -4,27 +4,35 @@ import Header from "../Header";
 
 import './style.css';
 
-import Banner from '../../assets/images/kinerabanner.png';
+import Banner from '../../assets/images/kinera-banner-renew.png';
 
 const Welcome = () => {
     return (
-        <div className="w-full mb-12 text-[#F3F6FB]"
-            id="home"
-        >
-
+        <div className="w-full mb-12 text-[#F3F6FB]" id="home">
             <Header />
-
             <div className="main-content">
                 <div className="content-wrapper">
                     <h1 className="kinera-title">THE KINERA PLATFORM</h1>
 
-                    <div className="logo-section">
+                    <div className="logo-section mt-10">
                         <img src={Banner} alt='BigK02' className="logo-img pulse" />
-                        
                     </div>
-                    <p className="text-[19px] text-center mt-10" ><span class="glitch">We are</span> a decentralised media platform where users and creators publish their content; help rank and curate videos and films from public repositories and from other media networks on the Internet. </p>
-                    <p className="text-[19px] text-center -mt-0" > The process of rank and curation leads to a set of public lists with relevant content that, in a second interaction, will be used to create television channels. </p>
-                    <p className="text-[19px] text-center -mt-0" > Kineras' mission is to harness the wisdom of the crowd to bring into focus new media narratives, pieces from creators that mirror the complexity and creativity of the world we live in.  </p>
+
+                    <p className="text-[19px] text-center">
+                        <span className="glitch">We are</span> a decentralised media platform where users and creators publish<br />
+                        their content; help rank and curate videos and films<br />
+                        from public repositories and from other media networks on the Internet.
+                    </p>
+
+                    <p className="text-[19px] text-center -mt-0">
+                        The process of rank and curation leads to a set of public lists with relevant content that,<br />
+                        in a second interaction, will be used to create television channels.
+                    </p>
+
+                    <p className="text-[19px] text-center -mt-0">
+                        Kineras' mission is to harness the wisdom of the crowd to bring into focus new media narratives,<br />
+                        pieces from creators that mirror the complexity and creativity of the world we <br /> live in.
+                    </p>
                 </div>
 
                 <div className="button-wrapper">
