e8b3def3f87bfdfc6047f4f65f01216b2faeb66f
gitlab_kine_leandro <leandro@ipa.kine.network>
2024-01-31 18:31:01 -0300

    links

--- src/components/Blog/blogPosts.js
+++ src/components/Blog/blogPosts.js
@@ -3,14 +3,14 @@ const blogPosts = [
       id: 1,
       title: "KINERA; A PLATFORM FOR INDEPENDENT MEDIA CREATION (part 1)",
       excerpt: "We live in the shadow of Artificial Intelligence's...",
-      url: "https://main--incredible-wisp-737646.netlify.app/(part%201)",
+      url: "https://incredible-wisp-737646.netlify.app/(part%201)",
       thumbnail: "https://main--incredible-wisp-737646.netlify.app/images/kinera-blog-4.png"
     },
     {
       id: 2,
       title: "KINERA; A PLATFORM FOR INDEPENDENT MEDIA CREATION (part 2)",
       excerpt: "The Social Media Paradox When a new wave of technology...",
-      url: "https://main--incredible-wisp-737646.netlify.app/(part%202)",
+      url: "https://incredible-wisp-737646.netlify.app/(part%202)",
       thumbnail: "https://main--incredible-wisp-737646.netlify.app//images/kinera-blog-3.png"
     }
     // Add more posts as needed
