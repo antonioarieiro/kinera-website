652645a1e48a11a2176f6fa51117d1971ca061bf
gitlab_kine_leandro <leandro@ipa.kine.network>
2024-06-17 20:57:26 -0300

    white kinera2

--- src/components/Funding/index.jsx
+++ src/components/Funding/index.jsx
@@ -22,32 +22,32 @@ const Funding = () => {
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         From a time when venture capital didn’t exist to the current funding landscape, we can see the diminishing role of individual investors and the comeback of centralised corporate models.
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         Before NFTs captured the world’s attention, crypto projects were mostly protocols and financial applications. NFTs represented a shift in blockchain technology as tokens began to be employed to serve practical purposes in people's lives. We anticipate this trend to persist.
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         <span className="glitch">The Kinex token </span>and curation markets create a gamified environment to classify information at scale. Because Kinera has no entry barriers and the token has practical purposes in social life, we expect it to emerge with the same capacity to capture people's imagination as NFTs.
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         To ensure that voting systems remain unbiased against users with initial advantages in voting power, seed funding investors, consultants or other contributors aren’t paid with Kinex tokens (or are subject to a limited amount). Our funding event will be open to everyone, and contributions will be capped.
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         A community of interested users committed to innovation using and advocating for the concepts outlined in the present Paper is taking its first steps. This text is an open invitation to collaboration. You can be a beta tester, join the technical team coding the application, or contribute with new ideas to shape the ecosystem. We pursue common goals, support common rules and take risks together.
                         </p>
                     </div>
                     <div className="card-funding" data-aos="fade-left">
-                        <p className="text-center text-black hover:text-white text-[19px]">
+                        <p className="text-center hover:text-white text-[19px] text-[#351e2d]">
                         <span className="glitch">Subscribe to our newsletter</span> if you wish to participate and receive regular information about the project or our funding rounds.
                         </p>
                     </div>
--- src/components/Participation/index.jsx
+++ src/components/Participation/index.jsx
@@ -29,28 +29,28 @@ const Participation = () => {
                     <div className="flex flex-col md:flex-row items-center gap-4" data-aos="fade-left">
                         <DropItem
                           question="Curators"
-                          answer={<p style={{ fontSize: '20px', color: '#000000' }}><strong>CURATORS</strong> have a central role; they are responsible for discovering and organising our constellations. Their activities include searching the internet and decentralised networks for top-tier content, organising competitions, and voting for the best channels. </p>}
+                          answer={<p style={{ fontSize: '20px', color: '#351e2d' }}><strong>CURATORS</strong> have a central role; they are responsible for discovering and organising our constellations. Their activities include searching the internet and decentralised networks for top-tier content, organising competitions, and voting for the best channels. </p>}
                         />
                         <img src={curators01} alt="curators01" className="w-[50%] md:w-[230px]" />
                     </div>
                     <div className="flex flex-col md:flex-row-reverse items-center gap-4" data-aos="fade-right">
                         <DropItem
                           question="Creators"
-                          answer={<p style={{ fontSize: '20px', color: '#000000' }}><strong>CREATORS</strong> are the members who craft distinctive and captivating content from our curated lists to create streaming channels. These channels may be further enhanced with interviews and live events, providing a comprehensive viewing experience.</p>}
+                          answer={<p style={{ fontSize: '20px', color: '#351e2d' }}><strong>CREATORS</strong> are the members who craft distinctive and captivating content from our curated lists to create streaming channels. These channels may be further enhanced with interviews and live events, providing a comprehensive viewing experience.</p>}
                         />
                         <img src={creators01} alt="creators01" className="w-[50%] md:w-[230px]" />
                     </div>
                     <div className="flex flex-col md:flex-row items-center gap-4" data-aos="fade-left">
                         <DropItem
                           question="Moderators"
-                          answer={<p style={{ fontSize: '20px', color: '#000000' }}><strong>MODERATORS</strong> are the volunteers who maintain the platform's high quality and safety standards. They report and resolve content disputes and ensure that all users follow guidelines.  You can set your moderation flag on if your reputation is in the top 80%. If so, report content, use your tokens to vote the decisions, and win tokens back from voting according to the rules. Kinex token rewards the individual's expertise and commitment to carry out the role with knowledge and depth.</p>}
+                          answer={<p style={{ fontSize: '20px', color: '#351e2d' }}><strong>MODERATORS</strong> are the volunteers who maintain the platform's high quality and safety standards. They report and resolve content disputes and ensure that all users follow guidelines.  You can set your moderation flag on if your reputation is in the top 80%. If so, report content, use your tokens to vote the decisions, and win tokens back from voting according to the rules. Kinex token rewards the individual's expertise and commitment to carry out the role with knowledge and depth.</p>}
                         />
                         <img src={moderators01} alt="moderators01" className="w-[50%] md:w-[230px]" />
                     </div>
                     <div className="flex flex-col md:flex-row-reverse items-center gap-4" data-aos="fade-right">
                         <DropItem
                           question="Ambassadors"
-                          answer={<p style={{ fontSize: '20px', color: '#000000' }}><strong>AMBASSADORS</strong> are volunteers who wish to see the growth and success of the community. They play a crucial role as mediators, advocates, and guardians of the ecosystem, fostering its expansion and protection and organising or promoting initiatives that strengthen the ecosystem. </p>}
+                          answer={<p style={{ fontSize: '20px', color: '#351e2d' }}><strong>AMBASSADORS</strong> are volunteers who wish to see the growth and success of the community. They play a crucial role as mediators, advocates, and guardians of the ecosystem, fostering its expansion and protection and organising or promoting initiatives that strengthen the ecosystem. </p>}
                         />
                         <img src={ambassadors01} alt="ambassadors01" className="w-[50%] md:w-[230px]" />
                     </div>
