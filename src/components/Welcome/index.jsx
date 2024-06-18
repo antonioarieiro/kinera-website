c22dae33a968a22d9cde99bfe4d5c2db72c92d1f
gitlab_kine_leandro <leandro@ipa.kine.network>
2024-06-18 19:00:34 -0300

    white kinera v2.1.1

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -16,15 +16,21 @@ const Welcome = () => {
                                 <img src={Banner} alt='BigK02' className="logo-img pulse" />
                             </div>
                             <h1 className="kinera-title" style={{ zIndex: 1, color: "#351e2d" }}>KINERA</h1>
-                            <p className="mt-6 text-base text-center" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d"  }}>
+                            <div className="card-home" data-aos="">
+                            <p className="mt-6 text-base text-justify" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d"  }}>
                                 <span className="glitch">We are</span> a decentralised media platform where users and creators publish their content; help rank and curate videos and films from public repositories and other media networks on the Internet.
                             </p>
-                            <p className="text-base text-center mt-6" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d" }}>
+                            </div>
+                            <div className="card-home" data-aos="">
+                            <p className="text-base text-justify mt-6" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d" }}>
                             The process of rank and curation produces publicly accessible lists filled with relevant content that participants can remix to create Streaming Channels.
                             </p>
-                            <p className="text-base text-center mt-6" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d"  }}>
+                            </div>
+                            <div className="card-home" data-aos="">
+                            <p className="text-base text-justify mt-6" style={{ zIndex: 1, fontSize: '16px', color: "#351e2d"  }}>
                                 <b> Kineras' mission is to harness the wisdom of the crowd to</b> bring focus into new media narratives, where creators can mirror the complexity and creativity of people's interactions in the world we live in.
                             </p>
+                            </div>
                         </div>
                     </div>
                 </div>
