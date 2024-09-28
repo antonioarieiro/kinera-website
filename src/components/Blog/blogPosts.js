3b1de9358e069e0ef1ec892a1623372fe7521830
gitlab_kine_leandro <leandro@ipa.kine.network>
2024-09-28 16:47:50 -0300

    changes a lot 2

--- src/components/Blog/blogPosts.js
+++ src/components/Blog/blogPosts.js
@@ -1,25 +1,25 @@
 const blogPosts = [
-    {
-      id: 1,
-      title: "Kinera: A Platform for Independent Media Creation (Part 1)",
-      excerpt: "We live in the shadow of Artificial Intelligence's...",
-      url: "https://blog.kinera.network/(part%201)",
-      thumbnail: "https://blog.kinera.network/images/kinera-blog-4.png"
-    },
-    {
-      id: 2,
-      title: "Kinera: A Platform for Independent Media Creation (Part 2)",
-      excerpt: "The Social Media Paradox When a new wave of technology...",
-      url: "https://blog.kinera.network/(part%202)",
-      thumbnail: "https://blog.kinera.network/images/kinera-blog-3.png"
-    },
-    {
-      id: 3,
-      title: "Presenting Kinera’s Minimum Viable Product",
-      excerpt: "After two years of study and development, the InvisibleHandLab...",
-      url: "https://blog.kinera.network/Kinera%20Platform",
-      thumbnail: "https://blog.kinera.network/images/kinera-blog-5.png"
-    }
-  ];
-  
-  export default blogPosts;
+  {
+    id: 1,
+    title: "The Rise of KINERA (Part 1)",
+    excerpt: "We live in the shadow of Artificial Intelligence's...",
+    url: "https://blog.kinera.network/(part%201)",
+    thumbnail: "https://blog.kinera.network/images/kinera-blog-4.png"
+  },
+  {
+    id: 2,
+    title: "The Rise of KINERA (Part 2)",
+    excerpt: "When a new wave of information technology emerges...",
+    url: "https://blog.kinera.network/(part%202)",
+    thumbnail: "https://blog.kinera.network/images/kinera-blog-3.png"
+  },
+  {
+    id: 3,
+    title: "Presenting Kinera’s Minimum Viable Product",
+    excerpt: "After two years of study and development, the InvisibleHandLab...",
+    url: "https://blog.kinera.network/Kinera%20Platform",
+    thumbnail: "https://blog.kinera.network/images/kinera-blog-5.png"
+  }
+];
+
+export default blogPosts;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const [showDropDownMenu, setShowDropDownMenu] = useState(false);
 
     return (
-        <header className="flex flex-col lg:flex-row items-center justify-center w-full px-10 py-2 border-b border-white bg-[#FFFFFF] text-[#3c1e11]">
+        <header className="flex flex-col lg:flex-row items-center justify-center w-full px-10 py-2 border-b border-white bg-[#FFFFFF] text-[#351e2d]">
             <div className="flex justify-between items-center w-full lg:w-auto min-h-[70px]">
                 <GiHamburgerMenu
                     className="lg:hidden cursor-pointer w-10 h-10"
@@ -16,11 +16,11 @@ const Header = () => {
             <nav className={`flex flex-col lg:flex-row items-center gap-4 lg:gap-8 mt-6 lg:mt-0 ${showDropDownMenu ? 'block' : 'lg:block hidden'} lg:flex`}>
                 <a href="/#home" className="text-base hover:text-[#6a466a]">Home</a>
                 <div className="group relative z-10">
-                    <button className="flex items-center gap-2 text-#3c1e11 group-hover:text-[#6a466a] text-base">
+                    <button className="flex items-center gap-2 text-#351e2d group-hover:text-[#6a466a] text-base">
                         Kinera
-                        <IoIosArrowDown className="text-[#6a466a] group-hover:text-[#3c1e11]" />
+                        <IoIosArrowDown className="text-[#6a466a] group-hover:text-[#351e2d]" />
                     </button>
-                    <div className="invisible group-hover:visible group-hover:opacity-100 rounded w-60 absolute left-0 top-full transition-all opacity-0 bg-[#3c1e11] shadow-xl py-1">
+                    <div className="invisible group-hover:visible group-hover:opacity-100 rounded w-60 absolute left-0 top-full transition-all opacity-0 bg-[#351e2d] shadow-xl py-1">
                         <ul>
                             <li><a className="text-[#FFFFFF] block px-4 py-2 hover:bg-[#6a466a]" href="/#about">About</a></li>
                             {/*<li><a className="text-[#FFFFFF] block px-4 py-2 hover:bg-[#6a466a]" href="/#mission">Mission</a></li>*/}
@@ -35,10 +35,10 @@ const Header = () => {
                 <a href="/#community" className="text-base hover:text-[#6a466a]">Community</a>
                 <a href="/#funding" className="text-base hover:text-[#6a466a]">Funding</a>
                 <a href="/#newsletter" className="text-base hover:text-[#F6a466aF0A78]">Newsletter</a>
-                {/* <a href="https://blog.kinera.network/blog" target="_blank" rel="noopener noreferrer" className="text-base hover:text-[#6a466a]">Blog</a> */}
+                <a href="https://blog.kinera.network/blog" target="_blank" rel="noopener noreferrer" className="text-base hover:text-[#6a466a]">Blog</a>
                 <a href="/#social" className="text-base hover:text-[#6a466a]">Social</a>
                 {/* New Button Added */}
-                <a href="https://alpha.kinera.network/" target="_blank" rel="noopener noreferrer" className="ml-4 px-4 py-2 bg-[#3c1e11] hover:bg-[#6a466a]/80 transition duration-300 ease-in-out rounded text-white">Launch dAPP</a>
+                <a href="https://alpha.kinera.network/" target="_blank" rel="noopener noreferrer" className="ml-4 px-4 py-2 bg-[#351e2d] hover:bg-[#6a466a]/80 transition duration-300 ease-in-out rounded text-white">Launch dAPP</a>
             </nav>
         </header>
     );
